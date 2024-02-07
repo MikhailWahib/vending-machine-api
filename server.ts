990c0b84933dc4fbcea058cf0ff1d097aa3cb866
MikhailWahib <mikhailwahib1@gmail.com>
2024-02-07 22:52:04 +0200

    Add root screen

--- server.ts
+++ server.ts
@@ -5,9 +5,9 @@ const app = createServer()
 const port = process.env.PORT || 3000
 
 app.get("/", (req, res) => {
-	res.json({
-		message: "Hello World!",
-	})
+	res.send(
+		"<p>For API documentation, go to <a target='_blank' href='https://github.com/MikhailWahib/vending-machine-api/blob/main/DOCS.md'>DOCS</a></p>"
+	)
 })
 
 app.listen(port, () => {
